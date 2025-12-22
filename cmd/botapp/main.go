71878b66d8524ab5c51e1026a2c44d6e7a40528f
jamaan01 <makspinyazenko8@gmail.com>
2025-12-22 17:29:16 +0200

    Feat: Finish main.go and update structure

--- cmd/botapp/main.go
+++ cmd/botapp/main.go
@@ -1,3 +1,36 @@
-package main 
+package main
 
-import 
+import (
+	"context"
+	"log/slog"
+	"os"
+	"tgbotcoach/internal/telegram"
+
+	th "github.com/mymmrac/telego/telegohandler"
+)
+
+func main() {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	}))
+	slog.SetDefault(logger)
+
+	bot := telegram.NewBot()
+	ctx, cancel := context.WithCancel(context.Background())
+
+	updates, err := bot.UpdatesViaLongPolling(ctx, nil)
+	if err != nil {
+		slog.Error("Бот не принимает айпдейты", "error", err)
+		os.Exit(1)
+	}
+
+	bh, err := th.NewBotHandler(bot, updates)
+	if err != nil {
+		slog.Error("Ошибка инициализации хендлера", "error", err)
+		os.Exit(1)
+	}
+
+	bh.Start()
+	defer bh.Stop()
+	defer cancel()
+}
---
+++ internal/telegram/connect.go
@@ -0,0 +1,18 @@
+package telegram
+
+import (
+	"log/slog"
+	"os"
+	"tgbotcoach/internal/config"
+
+	"github.com/mymmrac/telego"
+)
+
+func NewBot() *telego.Bot {
+	bot, err := telego.NewBot(config.LOad().BotToken, telego.WithDefaultDebugLogger())
+	if err != nil {
+		slog.Error("Не удалось создать бота", "error", err)
+		os.Exit(1)
+	}
+	return bot
+}
